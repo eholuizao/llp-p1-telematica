0ab7f96d53cd4f40925753cea2849cf9f6470606
Luiz Gabriel <bieel180360@gmail.com>
2023-05-19 20:18:33 -0300

    avaliacao 02 concluida

--- avaliacoes/avaliacao-02.c
+++ avaliacoes/avaliacao-02.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
-void print_matriz(int *matriz, int rows) {
+
+void imprimirMatriz(int *matriz, int size) {
 	int i, j;
 	
-    for(i = 0; i < rows; i++) {
-        for(j = 0; j < rows; j++) {
-            printf("%d ", *((matriz + i * rows) + j));
-        }
-        printf("\n");
-    }
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			/* 
+			O 3° placeholder é substituído por uma operação aritmética com ponteiros
+			que realiza a iteração sobre a matriz.
+			*/
+			printf("elemento [%d][%d] = %d \n", i, j, *((matriz + i * size) + j));
+		}
+	}
+}
+
+void somaMatriz(int *matrizA, int *matrizB, int *matrizC, int size) {
+	int i, j;
+	
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			*((matrizC + i * size) + j) = *((matrizA + i * size) + j) + *((matrizB + i * size) + j);
+		}
+	}
 }
 
 int main() {
-    int matriz[][3] = {{1, 2, 2}, {4, 5, 2}, {4, 5, 4}};
-    int tamanho = sizeof(matriz) / sizeof(matriz[0]);
-    
-    printf("%d", sizeof(matriz[0]));
- 
-    print_matriz((int *)matriz, tamanho);
-    return 0;
+	int matriz[][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+	int matriz2[][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+	int matriz3[][3] = {};
+	int tamanho = sizeof(matriz) / sizeof(matriz[0]);
+	
+	somaMatriz(matriz, matriz2, matriz3, tamanho);
+	imprimirMatriz(matriz3, tamanho);
+	
+	return 0;
 }
 
